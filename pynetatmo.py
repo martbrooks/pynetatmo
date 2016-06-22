54604c9b132bfa36a9dc16f7bbc23c14fee58d6e
Martin A. Brooks <martin@clues.ltd.uk>
2016-06-22 15:52:19 +0100

    make pylint happier.

--- pynetatmo.py
+++ pynetatmo.py
@@ -1,8 +1,8 @@
+''' A Python module to talk to Netatmo's weather station API. '''
+
 import datetime as dt
-import json
 import logging
 import os
-import pprint
 import requests
 import yaml
 
@@ -11,7 +11,10 @@ logger = logging.getLogger(__name__)
 
 class Weatherstation(object):
 
+    ''' The weatherstation class. '''
+
     def __init__(self, configyaml, loglevel=None):
+        ''' Initialise the object and set up the authentication tokens. '''
         numeric_level = getattr(logging, loglevel.upper(), None)
         logging.basicConfig(
             format='[%(asctime)s][%(levelname)s] %(message)s',
@@ -21,6 +24,7 @@ class Weatherstation(object):
         self._get_or_refresh_tokens()
 
     def _get_token(self):
+        ''' Fetch a completely new token. '''
         payload = {
             'grant_type': 'password',
             'client_id': self.config['client_id'],
@@ -28,30 +32,32 @@ class Weatherstation(object):
             'username': self.config['username'],
             'password': self.config['password'],
             'scope': 'read_station'}
-        r = requests.post(
+        response = requests.post(
             'https://api.netatmo.net/oauth2/token',
             data=payload)
-        return r.json()
+        return response.json()
 
     def _refresh_token(self):
+        ''' Current token is too old, refresh it. '''
         payload = {
             'grant_type': 'refresh_token',
             'client_id': self.config['client_id'],
             'client_secret': self.config['client_secret'],
             'refresh_token': self.config['refresh_token']
         }
-        r = requests.post(
+        response = requests.post(
             'https://api.netatmo.net/oauth2/token',
             data=payload)
-        return r.json()
+        return response.json()
 
     def _get_or_refresh_tokens(self):
+        ''' Make sure the token store is populated with a fresh access token.'''
         haschanged = False
         store = {}
         tokenstore = self.config['tokenstore']
 
         if os.path.isfile(tokenstore):
-            logger.debug('Reading stored tokens from \'%s\'.' % (tokenstore))
+            logger.debug('Reading stored tokens from \'%s\'.', tokenstore)
             stream = open(tokenstore, 'r')
             store = yaml.load(stream)
 
@@ -63,16 +69,13 @@ class Weatherstation(object):
                 store['tokens_last_updated'],
                 "%Y-%m-%dT%H:%M:%S.%f")
             diffsecs = (dt.datetime.utcnow() - lastupdate).total_seconds()
-            logger.debug('Token is %i seconds old.' % (diffsecs))
+            logger.debug('Token is %i seconds old.', diffsecs)
 
             if diffsecs > store['expires_in']:
                 logger.debug('Refreshing tokens.')
-                refresh_token = store['refresh_token']
                 newstore = self._refresh_token()
-                logger.debug('Old access token: %s.' % (store['access_token']))
-                logger.debug(
-                    'New access token: %s.' %
-                    (newstore['access_token']))
+                logger.debug('Old access token: %s.', store['access_token'])
+                logger.debug('New access token: %s.', newstore['access_token'])
                 store['access_token'] = newstore['access_token']
                 store['refresh_token'] = newstore['refresh_token']
                 store['tokens_last_updated'] = dt.datetime.utcnow().isoformat()
@@ -92,7 +95,7 @@ class Weatherstation(object):
         self.config['expires_in'] = store['expires_in']
 
         if haschanged:
-            logger.debug('Writing token store to \'%s\'.' % (tokenstore))
+            logger.debug('Writing token store to \'%s\'.', tokenstore)
             with open(tokenstore, 'w') as yaml_file:
                 yaml_file.write(yaml.dump(store, default_flow_style=False))
 
